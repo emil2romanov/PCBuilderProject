6dffff1e6f76ac81ed19f6f834da489a98cf8673
Emil Romanov <eromanov8@gmail.com>
2023-10-26 16:20:22 +0300

    adding binding models for user login and register

--- pc-builder/src/main/java/bg/softuni/pcbuilder/repository/UserRepository.java
+++ pc-builder/src/main/java/bg/softuni/pcbuilder/repository/UserRepository.java
@@ -4,6 +4,12 @@ import bg.softuni.pcbuilder.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findUserEntityByEmail(String email);
+
+    Optional<UserEntity> findUserEntityByUsername(String username);
 }
--- pc-builder/src/main/java/bg/softuni/pcbuilder/repository/UserRoleRepository.java
+++ pc-builder/src/main/java/bg/softuni/pcbuilder/repository/UserRoleRepository.java
@@ -1,9 +1,14 @@
 package bg.softuni.pcbuilder.repository;
 
 import bg.softuni.pcbuilder.model.entity.UserRoleEntity;
+import bg.softuni.pcbuilder.model.enums.UserRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRoleEntity, Long> {
+
+    Optional<UserRoleEntity> findUserRoleEntityByRole(UserRoleEnum role);
 }
